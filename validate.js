35e207fafa22edc7789a7d96ca7bec610d34c4af
Jad229 <76543236+Jad229@users.noreply.github.com>
2022-10-19 18:58:10 -0400

    Task: Verify Fields Changes: verify function complete

--- validate.js
+++ validate.js
@@ -1,9 +1,64 @@
 const form = document.querySelector("#seller-signup");
 
+const firstName = document.querySelector("#fname");
+const lastName = document.querySelector("#lname");
 const userPassword = document.querySelector("#password");
 const userID = document.querySelector("#id-num");
 const phoneNumber = document.querySelector("#phone-number");
 const email = document.querySelector("#email");
+const transaction = document.querySelector("#transactionType");
+
+const sellers =[
+    {
+        name:"Justin",
+        lname:"Diaz",
+        password:"pa$sw0Rd12",
+        ID:"12345671",
+    },
+    {
+        name:"Paul",
+        lname:"FireBlade",
+        password:"pa$sw0Rd12",
+        ID:"12345672",
+    },
+    {
+        name:"Destiny",
+        lname:"IceStorm",
+        password:"pa$sw0Rd12",
+        ID:"12345673",
+    },
+    {
+        name:"Penny",
+        lname:"LifeBringer",
+        password:"pa$sw0Rd12",
+        ID:"12345674",
+    },
+    {
+        name:"Leo",
+        lname:"DragonFlame",
+        password:"pa$sw0Rd12",
+        ID:"12345675",
+    },
+    {
+        name:"Scarlet",
+        lname:"ManaBloom",
+        password:"pa$sw0Rd12",
+        ID:"12345676",
+    },
+    {
+        name:"Astra",
+        lname:"GemKnight",
+        password:"pa$sw0Rd12",
+        ID:"12345677",
+    },
+    {
+        name:"Skye",
+        lname:"StarCaster",
+        password:"pa$sw0Rd12",
+        ID:"12345678",
+    }
+]
+
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
@@ -33,8 +88,25 @@ function validate(){
         alert("Please input a valid email address. Email must contain an @ followed by a period and an" +
             "email domain that consists of 2 to 5 characters.");
     }
+    else{
+        verify();
+    }
 }
 
 function verify(){
+    let matchFound = false;
+    sellers.forEach(function(seller){
+        if(firstName.value === seller.name && lastName.value === seller.lname
+        && userPassword.value === seller.password && userID.value === seller.ID){
+            matchFound = true;
+        }
+    })
 
+    if(matchFound){
+        alert(`Welcome ${firstName.value} ${lastName.value}! \n 
+        Transaction: ${transaction.options[transaction.selectedIndex].text}`);
+    }
+    else {
+        alert("No matching records found.");
+    }
 }
