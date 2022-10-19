2eda5344b7322f349e4642865356459458bbdd46
Jad229 <76543236+Jad229@users.noreply.github.com>
2022-10-18 21:29:58 -0400

    Task: Validate Fields Changes: validated email field

--- validate.js
+++ validate.js
@@ -1,14 +1,3 @@
-
-
-let passwordReg = /(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/;
-let idReg = /^[0-9]{8}$/;
-
-//10 digits which can be delineated either by spaces or dashes
-let phoneReg = /\d/ ;
-
-// must contain an @ followed by a period and an email domain that consists of 2 to 5 characters.
-let emailReg = /ab@./;
-
 const form = document.querySelector("#seller-signup");
 
 const userPassword = document.querySelector("#password");
@@ -25,6 +14,8 @@ form.addEventListener("submit", function(e){
 function validate(){
     let passwordReg = /(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/;
     let idReg = /^[0-9]{8}$/;
+    let phoneReg = /^[0-9]{3}\s?-?[0-9]{3}\s?-?[0-9]{4}$/;
+    let emailReg = /[\S]+@[a-zA-Z]{2,5}[\.][a-zA-Z]+/;
 
     if(userPassword.value.length > 10){
         alert("Password cannot exceed 10 characters");
@@ -32,11 +23,16 @@ function validate(){
     else if(!passwordReg.test(userPassword.value)){
         alert("Password must have at least 1 uppercase letter, 1 special character and 1 numeric character");
     }
-
-    if(!idReg.test(userID.value)){
+    else if(!idReg.test(userID.value)){
         alert("ID should only contain exactly an 8-digit number");
     }
-
+    else if(!phoneReg.test(phoneNumber.value)){
+        alert("phone number should consist of 10 digits which can be delineated either by spaces or dashes");
+    }
+    else if(!emailReg.test(email.value)){
+        alert("Please input a valid email address. Email must contain an @ followed by a period and an" +
+            "email domain that consists of 2 to 5 characters.");
+    }
 }
 
 function verify(){
