2da6e3742d38b495d2df73effb15e8c6f2acd107
Jad229 <jad229@njit.edu>
2022-10-20 20:44:53 -0400

    Task: Review Assignment 2 Changes: Blank submit, email confirmation, focus events

--- validate.js
+++ validate.js
@@ -22,13 +22,13 @@ const sellers =[
         ID:"12345672",
     },
     {
-        name:"destiny",
+        name:"Destiny",
         lname:"Icestorm",
         password:"pa$sw0Rd12",
         ID:"12345673",
     },
     {
-        name:"penny",
+        name:"Penny",
         lname:"Lifebringer",
         password:"pa$sw0Rd12",
         ID:"12345674",
@@ -59,34 +59,52 @@ const sellers =[
     }
 ]
 
-
 form.addEventListener("submit", function(e){
     e.preventDefault();
     validate();
 });
 
-
 function validate(){
+    let hasEmptyField = false;
     let passwordReg = /(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/;
     let idReg = /^[0-9]{8}$/;
     let phoneReg = /^[0-9]{3}\s?-?[0-9]{3}\s?-?[0-9]{4}$/;
     let emailReg = /\S+@[a-zA-Z]{2,5}[.][a-zA-Z]+/;
 
-    if(userPassword.value.length > 10){
+    for(let i = 0; i < 5; i++){
+        if(form[i].value === ''){
+            hasEmptyField = true;
+        }
+    }
+
+    if(hasEmptyField){
+        alert("Please fill in all the required fields.");
+    }
+    else if(userPassword.value.length > 10){
         alert("Password cannot exceed 10 characters");
+        form[2].focus();
     }
     else if(!passwordReg.test(userPassword.value)){
         alert("Password must have at least 1 uppercase letter, 1 special character and 1 numeric character");
+        form[2].focus();
     }
     else if(!idReg.test(userID.value)){
         alert("ID should only contain exactly an 8-digit number");
+        form[3].focus();
     }
     else if(!phoneReg.test(phoneNumber.value)){
         alert("phone number should consist of 10 digits which can be delineated either by spaces or dashes");
+        form[4].focus();
     }
-    else if(!emailReg.test(email.value)){
-        alert("Please input a valid email address. Email must contain an @ followed by a period and an" +
-            "email domain that consists of 2 to 5 characters.");
+    else if(document.querySelector("#emailConfirmation").checked){
+        if(!emailReg.test(email.value)){
+            alert("Please input a valid email address. Email must contain an @ followed by a period and an" +
+                "email domain that consists of 2 to 5 characters.");
+            form[5].focus();
+        }
+        else{
+            verify();
+        }
     }
     else{
         verify();
@@ -101,7 +119,7 @@ function verify(){
         && userPassword.value === seller.password && userID.value === seller.ID){
             matchFound = true;
         }
-    })
+    });
 
     if(matchFound){
         alert(`Welcome ${firstName.value} ${lastName.value}! \n 
